3f976c302a15a2c5faaf3595f124b66527567500
Gabe Ganberg <ganberg@aptima.com>
2023-06-20 09:47:56 -0700

    adding cookiecutter template

---
+++ template/simple-compose/{{cookiecutter.project_name}}/src/{{cookiecutter.python_package}}/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="{{cookiecutter.python_package}}",
+    version="{{cookiecutter.project_version}}",
+    description="{{cookiecutter.project_name}}",
+    install_requires=[
+        "yaada-core>={{cookiecutter.yaada_version}}",
+        "yaada-nlp>={{cookiecutter.yaada_version}}",
+        "yaada-dataset>={{cookiecutter.yaada_version}}",
+        "yaada-openapi>={{cookiecutter.yaada_version}}",
+        "yaada-webscraping>={{cookiecutter.yaada_version}}"
+    ],
+    python_requires=">=3.8.0",
+    packages=find_packages(),
+    scripts=[],
+)
